0006f45fbdf2e0877738762edad90078e8acd582
deakpal <attila.deakpal@chilisoft.org>
2015-07-28 17:19:28 +0200

    Test

---
+++ src/main/java/io/leaf/codegen/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package io.leaf.codegen;
+
+/**
+ * @author attila.deakpal
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
